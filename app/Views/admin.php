ac7b7c2850ed979b13430d8cbbded4f7dcb1adfb
OktovanAgung <oktovanagung@mhs.pelitabangsa.ac.id>
2023-06-05 16:43:24 +0700

    update Routes.php

---
+++ app/Views/admin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Halaman Admin</title>
+</head>
+
+<body>
+    <h1>Ini Halaman Admin</h1>
+    <!-- Tambahkan konten halaman admin sesuai kebutuhan Anda -->
+</body>
+
+</html>
