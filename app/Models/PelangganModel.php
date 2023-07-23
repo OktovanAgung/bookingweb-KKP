302b0e573fe292bdebd6c8c9a4719e1c0cc98725
OktovanAgung <oktovanagung@mhs.pelitabangsa.ac.id>
2023-07-23 19:57:50 +0700

    update 6 files

--- app/Models/PelangganModel.php
+++ app/Models/PelangganModel.php
@@ -9,4 +9,10 @@ class PelangganModel extends Model
     protected $table = 'tb_pelanggan';
     protected $primaryKey = 'id';
     protected $allowedFields = ['id', 'nama', 'no_whatsapp', 'nama_pet', 'notes', 'layanan', 'tanggal', 'waktu'];
+
+    public function updateStatusSelesai($orderId, $status)
+    {
+        $data = ['status_selesai' => $status];
+        $this->where('id', $orderId)->update($data);
+    }
 }
