23386d6651fd50b63b512332415da0017324fa93
OktovanAgung <oktovanagung@mhs.pelitabangsa.ac.id>
2023-07-29 22:01:08 +0700

    update 8 files

--- app/Views/calender.php
+++ app/Views/calender.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Edu+SA+Beginner:wght@400;500&family=Josefin+Sans:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?= base_url('assets/style/calender.css') ?>">
-    <link rel="stylesheet" href="<?= base_url('assets/style/calender_mobile.css') ?>">
     <title>Datepicker</title>
 </head>
 
